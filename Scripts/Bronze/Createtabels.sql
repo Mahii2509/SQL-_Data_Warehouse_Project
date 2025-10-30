52e795578ad7bde1e981cc739c2d723daf028c5e
Mahii2509 <mahendrachowdary36@gmail.com>
2025-10-30 11:03:25 +0530

    Update Createtabels.sql

--- Scripts/Bronze/Createtabels.sql
+++ Scripts/Bronze/Createtabels.sql
@@ -39,32 +39,6 @@ CREATE TABLE Bronze.crm_sales_details (
 
 
 
-CREATE TABLE Bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
-);
-
-
-
-CREATE TABLE Bronze.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
-);
-
-
-
 CREATE TABLE Bronze.erp_loc_a101 (
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50)
